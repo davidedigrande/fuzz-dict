dbff19836f210d516d4df23c7e8030dfe1ab219c
davidedigrande <davidedigrande.dev@gmail.com>
2022-02-12 13:35:13 +0100

    First release

---
+++ fuzdict/fuzdict.py
@@ -0,0 +1,67 @@
+from typing import Any
+from fuzzywuzzy import process
+
+
+class FuzDict(dict):
+    def __new__(cls, threshold:int=80, dict:dict={}, get_only:bool=False, *args, **kwargs):
+        if not type(threshold) is int:
+            raise ValueError("Threshold must be an integer.")
+
+        if 0 >= threshold >= 100:
+            return dict
+
+        else:
+            return super(FuzDict, cls).__new__(cls, threshold, dict, get_only, *args, **kwargs)
+
+    def __init__(self, threshold:int=80, dict:dict={}, get_only:bool=False, *args, **kwargs):
+        self.threshold = threshold
+        self.get_only = get_only
+        super().__init__(*args, **kwargs)
+        self.update(dict)
+         
+    def __getitem__(self, __k:str) -> Any:
+        if type(__k) is not str:
+            raise ValueError(f"{type(self)} only supports string objects as keys. Received {type(__k)} instead.")
+
+        value = super().get(__k, None)
+        if value:
+            return value
+
+        match, score = process.extractOne(__k, self.keys()) or ("", 0)
+        if score > self.threshold:
+            return super().__getitem__(match)
+        
+        raise KeyError(__k)
+    
+    def __setitem__(self, __k:str, v:Any) -> None:
+        if type(__k) is not str:
+            raise ValueError(f"{type(self)} only supports string objects as keys. Received {type(__k)} instead.")
+
+        if self.get_only:
+            return super().__setitem__(__k, v)
+
+        match, score = process.extractOne(__k, self.keys()) or ("", 0)
+        if score > self.threshold:
+            return super().__setitem__(match, v)
+
+        return super().__setitem__(__k, v)
+    
+    def get(self, __k:str, __default:Any=None, exact:bool=False):
+        if type(__k) is not str:
+            raise ValueError(f"{type(self)} only supports string objects as keys. Received {type(__k)} instead.")
+
+        value = super().get(__k, None)
+        if value:
+            return value
+
+        if exact:
+            return value or __default
+
+        match, score = process.extractOne(__k, self.keys()) or ("", 0)
+        if score > self.threshold:
+            return super().__getitem__(match)
+        
+        return __default
+    
+
+
--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.6' 
 DESCRIPTION = 'A Python dictionary with fuzzy-matched keys.'
 LONG_DESCRIPTION = 'A Python dictionary with fuzzy-matched keys.'
 
-with open('requirements.txt') as requirements:
-    requirements = requirements.readlines()
-
-# Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
-        name="fuzzydict", 
+        name="fuzdict", 
         version=VERSION,
         author="Davide Di Grande",
         author_email="davidedigrande94@gmail.com",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=requirements,         
+        install_requires=["fuzzywuzzy==0.18.0"],         
         keywords=['python'],
 )
