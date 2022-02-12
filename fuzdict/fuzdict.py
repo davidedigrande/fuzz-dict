1b3b1c24369fbf1eac02d695e93634d730a34053
davidedigrande <davidedigrande.dev@gmail.com>
2022-02-12 19:52:27 +0100

    Update docstrings

--- fuzdict/fuzdict.py
+++ fuzdict/fuzdict.py
@@ -5,7 +5,8 @@ from fuzzywuzzy import process
 class FuzDict(dict):
     """
     FuzDict is a simple python dictionary that implements Fuzzy Logic as a way to match keys.
-    This is implies that suitable values for creating keys are only of str type.
+    This implies that suitable values for creating keys are only of string type.
+    It also implies that at any time, the dictionary cannot have two or more similar strings, which means in turn that the input order matters.
     """
     def __init__(self, threshold:int=80, get_only:bool=False):
         self._threshold = threshold
