b8d634d3f1e55d6b809a61796db1bae2311ae374
davidedigrande <davidedigrande.dev@gmail.com>
2022-02-12 19:56:08 +0100

    Remove unused

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from fuzdict.fuzdict import FuzDict
 
 from fuzdict import FuzDict
 import requests
-import random
 
 
 dishes = requests.get("https://random-data-api.com/api/food/random_food?size=100", headers={"Content-type": "application/json"}).json()
