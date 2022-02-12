f9631e63165a4b0b64c64a676630799d0bbf68e8
davidedigrande <davidedigrande.dev@gmail.com>
2022-02-12 19:53:05 +0100

    Refactoring

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.6' 
+NAME = "fuzdict"
+VERSION = '0.0.1' 
 DESCRIPTION = 'A Python dictionary with fuzzy-matched keys.'
-LONG_DESCRIPTION = 'A Python dictionary with fuzzy-matched keys.'
+LONG_DESCRIPTION = \
+"""
+FuzDict is a simple python dictionary that implements Fuzzy Logic as a way to match keys.
+This implies that suitable values for creating keys are only of string type.
+It also implies that at any time, the dictionary cannot have two or more similar strings, which means in turn that the input order matters.
+"""
+AUTHOR="Davide Di Grande"
+EMAIL = "davidedigrande94@gmail.com"
 
 setup(
-        name="fuzdict", 
+        name=NAME, 
         version=VERSION,
-        author="Davide Di Grande",
-        author_email="davidedigrande94@gmail.com",
+        author=AUTHOR,
+        author_email=EMAIL,
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
