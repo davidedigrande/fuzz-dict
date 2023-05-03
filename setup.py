b03abbd106b86fd4a70d36417a9f6980544255e1
DAVIDE DI GRANDE <davidedigrande94@gmail.com>
2023-05-03 10:24:30 +0200

    Minor improvements

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup, find_packages
+from pathlib import Path
+
 from pip._internal.req import parse_requirements
+from setuptools import find_packages, setup
 
 setup(
-    name="fuzz-dict", 
-    version="0.0.1" ,
+    name="fuzz-dict",
+    version="0.0.3",
     author="Davide Di Grande",
     author_email="davidedigrande.dev@gmail.com",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=[ir.requirement for ir in parse_requirements('requirements.txt', session=False)],
+    install_requires=[ir.requirement for ir in parse_requirements("requirements.txt", session=False)],
+    project_urls={"Source": "https://github.com/davidedigrande/fuzz-dict"},
 )
